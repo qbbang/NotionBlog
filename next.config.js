ed8aad31305ce73973e7145e33e71c04ab455e69
qbbang <qbbang@gmail.com>
2021-04-27 16:55:27 +0900

    add: aasa 추가
    - 앱클립 테스트를 위한 AASA
    - https://github.com/vercel/next.js/discussions/10648
    - 로컬테스트 정상, http://localhost:3000/.well-known/apple-app-site-association

--- next.config.js
+++ next.config.js
@@ -9,3 +9,19 @@ module.exports = withBundleAnalyzer({
     domains: ['pbs.twimg.com']
   }
 })
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json'
+          }
+        ]
+      }
+    ]
+  }
+}
